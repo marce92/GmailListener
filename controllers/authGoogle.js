aa8ed7d3ab1840c414151940389f3137e8ffd38e
Joao Cunha <joao.marcelo.silva.cunha@gmail.com>
2018-03-10 19:13:37 +0000

    Google Auth

--- controllers/authGoogle.js
+++ controllers/authGoogle.js
@@ -1,9 +1,31 @@
 "use strict";
-
-var self = this;
+var _ = require('underscore');
+var googleAuth = require('google-auth-library');
 
 function authMethod(object){
-    console.log(object);
+    let decoded_object = decodeObject(object);
+    let auth = new googleAuth();
+
+    let clientId = (!_.isUndefined(decoded_object.client_id)) ? decoded_object.client_id : '';
+    let clientSecret = (!_.isUndefined(decoded_object.client_secret)) ? decoded_object.client_secret : '';
+    let clientUrl = (!_.isUndefined(decoded_object.redirect_uris[0])) ? decoded_object.redirect_uris[0] : '';
+
+    try {
+        let oauth2Client = new auth.OAuth2(clientId, clientSecret, clientUrl);
+
+        console.log(oauth2Client);
+    } catch(err) {
+        console.log('Error on Google authentication');
+    }
+}
+
+
+function decodeObject(object) {
+    let main = JSON.parse(object);
+
+    main = (!_.isUndefined(main.installed)) ? main.installed : {};
+
+    return main;
 }
 
 module.exports = {
--- index.js
+++ index.js
@@ -2,4 +2,7 @@
 
 let fs = require('fs');
 let auth = require('./controllers/authGoogle.js');
-fs.readFile('data/auth-data.json', 'utf-8',(err, data) => auth.authMethod(data));
+fs.readFile('data/auth-data.json', 'utf-8', (err, data) => auth.authMethod(data));
+
+
+// console.log('In authentication process!!');
